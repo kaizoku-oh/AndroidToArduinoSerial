b82acefd52263b08a04555c96124133f0782ccb8
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2022-02-11 14:38:45 +0100

    WIP

--- app/src/main/java/com/example/morf/MainActivity.java
+++ app/src/main/java/com/example/morf/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.morf;
 import static co.intentservice.chatui.models.ChatMessage.Type.RECEIVED;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -29,12 +36,16 @@ public class MainActivity extends AppCompatActivity implements ArduinoListener {
     private Arduino serialPort;
     private ChatView chatView;
 
+    NotificationCompat.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         chatView = findViewById(R.id.chat_view);
+
+        createNotificationChannel();
 
         serialPort = new Arduino(this);
         serialPort.setBaudRate(BAUDRATE);
@@ -62,6 +73,36 @@ public class MainActivity extends AppCompatActivity implements ArduinoListener {
                 Log.i(TAG, "userStoppedTyping: Stopped typing");
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "New message";
+            String description = "Received a new message";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
     }
 
     @Override
@@ -99,6 +140,10 @@ public class MainActivity extends AppCompatActivity implements ArduinoListener {
         message = new String(bytes);
         tsLong = System.currentTimeMillis();
         runOnUiThread(() -> chatView.addMessage(new ChatMessage(message, tsLong, RECEIVED)));
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(123, builder.build());
     }
 
     @Override
